45c73cc9b30d5bc5ccbfe412adff6b542a53621d
Ansari Mohammad Izan <izanansari123@gmail.com>
2024-03-11 23:19:06 +0530

    Changes

--- controller/index.js
+++ controller/index.js
@@ -111,3 +111,27 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 
+  function searchTable() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.querySelector('.search-input');
+    filter = input.value.toUpperCase();
+    table = document.getElementById('myTable');
+    tr = table.getElementsByTagName('tr');
+
+    // Loop through all table rows, and hide those that don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName('td');
+        for (var j = 0; j < td.length; j++) {
+            if (td[j]) {
+                txtValue = td[j].textContent || td[j].innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = '';
+                    break;
+                } else {
+                    tr[i].style.display = 'none';
+                }
+            }
+        }
+    }
+}
